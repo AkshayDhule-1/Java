0bc18b226cae481ca141cf822089f601f7c5319d
Akshay Dhule <88124942+AkshayDhule-1@users.noreply.github.com>
2023-07-27 17:39:16 +0530

    Update TreeMapTest.java

--- TreeMapTest.java
+++ TreeMapTest.java
@@ -1,90 +1,74 @@
 import java.util.Iterator;
-import java.util.TreeSet;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeMap;
 
-class ChemicalElement implements Comparable<ChemicalElement>
+class Country
 {
-	int atomicNumber;
-	String atomicName;
-	String atomicFormula;
-	float atomicWeight;
+	String name;
+	String capital;
+	String primeMinister;
+	String population;
+	String currency;
 	
-//	@Override
-//	public int compareTo(ChemicalElement o) { //k1.kiteFifght(k2)
-//		System.out.println("Comparing "+atomicNumber+" with "+o.atomicNumber);
-//		System.out.println("-------------");
-//		return Integer.compare(o.atomicNumber, atomicNumber);
-//	
-//	}
-	
-//	@Override
-//	public int compareTo(ChemicalElement o) { //k1.kiteFifght(k2)
-//		System.out.println("Comparing "+atomicName+" with "+o.atomicName);
-//		System.out.println("-------------");
-//		return atomicName.compareTo(o.atomicName);
-//	
-//	}
-	
-	@Override
-	public int compareTo(ChemicalElement o) { //k1.kiteFifght(k2)
-		System.out.println("Comparing "+atomicWeight+" with "+o.atomicWeight);
-		System.out.println("-------------");
-		return Float.compare(atomicWeight, o.atomicWeight);
-	
-	}
-	public ChemicalElement(int atomicNumber, String atomicName, String atomicFormula, float atomicWeight) {
+	public Country(String name, String capital, String primeMinister, String population, String currency) {
 		super();
-		this.atomicNumber = atomicNumber;
-		this.atomicName = atomicName;
-		this.atomicFormula = atomicFormula;
-		this.atomicWeight = atomicWeight;
+		this.name = name;
+		this.capital = capital;
+		this.primeMinister = primeMinister;
+		this.population = population;
+		this.currency = currency;
 	}
+
 	@Override
 	public String toString() {
-		return "ChemicalElement [atomicNumber=" + atomicNumber + ", atomicName=" + atomicName + ", atomicFormula="
-				+ atomicFormula + ", atomicWeight=" + atomicWeight + "]";
+		return "Country [name=" + name + ", capital=" + capital + ", primeMinister=" + primeMinister + ", population="
+				+ population + ", currency=" + currency + "]";
 	}
-	
 	
 	
 }
-public class TreeSetTest {
+public class TreeMapTest {
+
 	public static void main(String[] args) {
-		ChemicalElement element1 = new ChemicalElement(8, "Oxygen", "O", 15.999f);
-		ChemicalElement element2 = new ChemicalElement(2, "Helium", "He", 4.0026f);
-		ChemicalElement element3 = new ChemicalElement(1, "Hydrogen", "H", 1.008f);
+		Country c1 = new Country("India", "New Delhi", "Mr. Narendra Modi", "136.64 Crores", "Rs");
+		Country c2 = new Country("Pakistan", "Islamabad", "Mr. Imran Khan", "21.66 Crores", "Pakistani Rupee");
+		Country c3 = new Country("China", "Beijing", "Mr. Xi Jinping", "139.77 Crores", "Yuan");
+		Country c4 = new Country("England", "London", "Mr. Boriss Johnson", "6.66 Crores", "Pound Sterling");
+		Country c5 = new Country("America", "Washington DC", "Mr. Joe Biden", "32.82 Crores", "USD");
 		
-		ChemicalElement element4 = new ChemicalElement(14, "Silicon", "Si", 28.085f);
-		ChemicalElement element5 = new ChemicalElement(33, "Arsenic", "As", 74.92f);
-		System.out.println("Elements are created....");
+		TreeMap<String,Country> countryMap = new TreeMap<String,Country>();
+		System.out.println("Map is ready....");
 		
-		TreeSet periodicTableSet = new TreeSet();
-		System.out.println("Empty Periodic Table is created....");
+		countryMap.put("IND", c1);
+		countryMap.put("PAK", c2);
+		countryMap.put("CHI", c3);
+		countryMap.put("UK", c4);
+		countryMap.put("US", c5);
 		
-		System.out.println("NOW ..Adding the first element...");
-		periodicTableSet.add(element1);
-		
-		System.out.println("NOW ..Adding the second element...");
-		periodicTableSet.add(element2);
-		
-		System.out.println("NOW ..Adding the third element...");
-		periodicTableSet.add(element3);
-		
-		System.out.println("NOW ..Adding the fourth element...");
-		periodicTableSet.add(element4);
-		
-		System.out.println("NOW ..Adding the fifth element...");
-		periodicTableSet.add(element5);
-		
-		System.out.println("Added all the elements...");
-		
-		//Reward reward			 = magician.doTrick();
-		Iterator elementIterator =  periodicTableSet.iterator();
-		
-		while(elementIterator.hasNext()) {
-			ChemicalElement theElement = (ChemicalElement) elementIterator.next();
-			System.out.println("Element : "+theElement );
+		System.out.println("keys and values are added ....");
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter any key : ");
+		String theKey = scan.next();
+		if(countryMap.containsKey(theKey))
+		{
+			Country theCountry = countryMap.get(theKey);
+			System.out.println("The Country "+theCountry);
+		}
+		else {
+			System.out.println("Key Not Found ");
 		}
 		
+		Set<String> allKeys = countryMap.keySet();
+		Iterator<String> keysIterator =  allKeys.iterator();
 		
+		while(keysIterator.hasNext()) {
+			String key = keysIterator.next();
+			System.out.println("Key is : "+key);
+			Country foundCountry = countryMap.get(key);
+			System.out.println("Country is : "+foundCountry);
+		}
+
 	}
+
 }
